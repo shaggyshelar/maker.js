e17a3d125514bbfc1f757a307a3baf3d0a31d847
shaggyshelar <shaggy.shelar@gmail.com>
2019-06-09 15:29:06 +0530

    Completed row base with columns

--- docs/demos/js/custom/rows-base.js
+++ docs/demos/js/custom/rows-base.js
@@ -1,50 +1,61 @@
+var singleCellWidth = 3;
+var columnSpacerWidth = 10;
+var cellColumnSpacerWidth = 5;
+var singleColumnWidth = 2 * singleCellWidth + cellColumnSpacerWidth;
+
+var singleCellHeight = 9;
+var rowSpacerHeight = 10;
+var cellRowSpacerHeight = 5;
+var singleRowHeight = 2 * singleCellHeight + cellRowSpacerHeight;
+
 function getParameterDefinitions() {
     return [
-        { name: 'unitSize', type: 'int', initial: 2, caption: 'Unit Size' },
         { name: 'baseBoxColor', type: 'color', initial: '#F6D55C', caption: 'Base Box Color' },
+        { name: 'columnColor', type: 'color', initial: '#ED553B', caption: 'Column Color' },
         { name: 'totalRows', type: 'int', initial: 1, caption: 'Total Rows' },
         { name: 'totalColumns', type: 'int', initial: 1, caption: 'Total Columns' }
     ];
-  }
+}
 
-  function getTopBoxes(params) {
-    var records = [];
-      for(var i =0; i< params.totalRecords;i++) {
-        var row =  color(html2rgb(params.color),
-              difference(
-                  union(
-                      //base
-                    cube({size: [params.unitSize * 9, params.unitSize * 7, params.unitSize * 0.5]}),
-                    // Top boxes
-                    cube({size: [params.unitSize * 9, params.unitSize , params.unitSize * 2]}).translate([0,params.unitSize * 7 - params.unitSize,0])
-                  )
+function getDummyColumns(params) {
+    var columns = [];
+    var height = params.totalRows * singleRowHeight + params.totalRows * rowSpacerHeight;
+    for (var i = 0; i < params.totalColumns; i++) {
+        var leftColumn = color(html2rgb(params.columnColor),
+            difference(
+                union(
+                    cube({ size: [singleCellWidth, height, 7] })
+                )
             )
-        ).translate([i * params.unitSize * 9, 0, 0]);
-        records.push(row);
-      }
-      return records;
-  }
+        ).translate([columnSpacerWidth + i * singleColumnWidth + i * columnSpacerWidth, 0, 0.5]);
+        columns.push(leftColumn);
+
+        var rightColumn = color(html2rgb(params.columnColor),
+            difference(
+                union(
+                    cube({ size: [singleCellWidth, height, 7] })
+                )
+            )
+        ).translate([columnSpacerWidth + i * (singleColumnWidth + columnSpacerWidth) + (singleCellWidth +cellColumnSpacerWidth), 0, 0.5]);
+        columns.push(rightColumn);
+    }
+    return columns;
+}
+
+function getBase(params) {
+    var height = params.totalRows * singleRowHeight + params.totalRows * rowSpacerHeight;
+    var width = params.totalColumns * singleColumnWidth + params.totalColumns * columnSpacerWidth + columnSpacerWidth;
+    return color(html2rgb(params.baseBoxColor),
+        cube({
+            size: [width,height,0.5]
+        })
+    );
+}
 
 function main(params) {
-    var singleCellWidth = 3;
-    var columnSpacerWidth = 10;
-    var cellColumnSpacerWidth = 5;
-    var singleColumnWidth = 2 * singleCellWidth + cellColumnSpacerWidth;
+    var dummyColumns = getDummyColumns(params);
+    var baseBox = getBase(params);
 
-    var singleCellHeight = 9;
-    var rowSpacerWidth = 10;
-    var cellRowSpacerWidth = 5;
-    var singleRowWidth = 2 * singleCellHeight + cellRowSpacerWidth;
-
-    var records = [];
-    var baseBox = color(html2rgb(params.baseBoxColor),
-        cube({ size: [
-            params.totalColumns * singleColumnWidth + params.totalColumns * columnSpacerWidth, 
-            params.totalRows * singleRowWidth + params.totalRows * rowSpacerWidth, 
-            0.5] })
-    );
-
-    records.push(baseBox);
-    return records;
+    dummyColumns.push(baseBox);
+    return dummyColumns;
 }
-  
