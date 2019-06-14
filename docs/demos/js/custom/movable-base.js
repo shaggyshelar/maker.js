6d1c9d01d1cb644527e9559e451fdf4aec478d2d
shaggyshelar <shaggy.shelar@gmail.com>
2019-06-14 11:51:37 +0530

    Completed movable base for 3 pin

--- docs/demos/js/custom/movable-base.js
+++ docs/demos/js/custom/movable-base.js
@@ -8,39 +8,75 @@ function getParameterDefinitions() {
         { name: 'totalRecords', type: 'int', initial: 1, caption: 'Total Records' }
     ];
 }
+
+function getData(params, totalWidth) {
+    var spacerWidth = params.unitSize * params.rowBaseSpacerSize + 0.5;
+    var rightPinStart = spacerWidth + params.spaceBetweenPins + 2 * params.unitSize;
+
+    if (params.isTwoPin) {
+        return union(
+            // Base
+            cube({ size: [params.unitSize * totalWidth + params.unitSize, params.unitSize * 4, params.unitSize * 0.5] }),
+
+            // Top boxes
+            cube({ size: [spacerWidth, params.unitSize * 0.5, params.unitSize] }).translate([0, params.unitSize * 4 - params.unitSize * 0.5, 0]),
+            cube({ size: [params.spaceBetweenPins, params.unitSize * 0.5, params.unitSize] }).translate([spacerWidth + params.unitSize, params.unitSize * 4 - params.unitSize * 0.5, 0]),
+            cube({ size: [spacerWidth, params.unitSize * 0.5, params.unitSize] }).translate([rightPinStart, params.unitSize * 4 - params.unitSize * 0.5, 0]),
+
+            // bottom boxes
+            difference(
+                cube({ size: [spacerWidth, params.unitSize * 3, params.unitSize * 1.5] }).translate([0, 0, 0]),
+                cube({ size: [params.unitSize * 0.5, params.unitSize * 2, params.unitSize * 3] }).translate([spacerWidth - 0.5, params.unitSize / 2, 0])
+            ),
+
+            // center box
+            cube({ size: [params.unitSize * params.spaceBetweenPins, params.unitSize * 3, params.unitSize * 1.5] }).translate([spacerWidth + params.unitSize, 0, 0]),
+
+            difference(
+                cube({ size: [spacerWidth, params.unitSize * 3, params.unitSize * 1.5] }).translate([rightPinStart, 0, 0]),
+                cube({ size: [params.unitSize * 0.5, params.unitSize * 2, params.unitSize * 3] }).translate([rightPinStart, params.unitSize / 2, 0])
+            )
+        );
+    }
+
+    return union(
+        // Base
+        cube({ size: [params.unitSize * totalWidth + params.unitSize, params.unitSize * 4, params.unitSize * 0.5] }),
+
+        // Top boxes
+        cube({ size: [spacerWidth, params.unitSize * 0.5, params.unitSize] }).translate([0, params.unitSize * 4 - params.unitSize * 0.5, 0]),
+        cube({ size: [params.spaceBetweenPins, params.unitSize * 0.5, params.unitSize] }).translate([spacerWidth + params.unitSize, params.unitSize * 4 - params.unitSize * 0.5, 0]),
+        cube({ size: [params.spaceBetweenPins, params.unitSize * 0.5, params.unitSize] }).translate([rightPinStart, params.unitSize * 4 - params.unitSize * 0.5, 0]),
+        cube({ size: [spacerWidth, params.unitSize * 0.5, params.unitSize] }).translate([rightPinStart + params.spaceBetweenPins + params.unitSize, params.unitSize * 4 - params.unitSize * 0.5, 0]),
+
+        // bottom boxes
+        difference(
+            cube({ size: [spacerWidth, params.unitSize * 3, params.unitSize * 1.5] }).translate([0, 0, 0]),
+            cube({ size: [params.unitSize * 0.5, params.unitSize * 2, params.unitSize * 3] }).translate([spacerWidth - 0.5, params.unitSize / 2, 0])
+        ),
+
+        // center box
+        cube({ size: [params.unitSize * params.spaceBetweenPins, params.unitSize * 3, params.unitSize * 1.5] }).translate([spacerWidth + params.unitSize, 0, 0]),
+        cube({ size: [params.unitSize * params.spaceBetweenPins, params.unitSize * 3, params.unitSize * 1.5] }).translate([rightPinStart, 0, 0]),
+
+        difference(
+            cube({ size: [spacerWidth, params.unitSize * 3, params.unitSize * 1.5] }).translate([rightPinStart + params.spaceBetweenPins + params.unitSize, 0, 0]),
+            cube({ size: [params.unitSize * 0.5, params.unitSize * 2, params.unitSize * 3] }).translate([rightPinStart + params.spaceBetweenPins + params.unitSize, params.unitSize / 2, 0])
+        )
+    );
+}
+
 function main(params) {
     var records = [];
     var numberOfPins = params.isTwoPin ? 2: 3;
     var totalWidth = (params.unitSize * 2 * params.rowBaseSpacerSize) + 
         (params.unitSize * numberOfPins) + 
         (params.unitSize * (numberOfPins - 1) * params.spaceBetweenPins);
-    var spacerWidth = params.unitSize * params.rowBaseSpacerSize + 0.5;
-    var rightPinStart = spacerWidth + params.spaceBetweenPins + 2 * params.unitSize;
+    
+    
     for (var i = 0; i < params.totalRecords; i++) {
         var row = color(html2rgb(params.movableBaseColor),
-            union(
-                // Base
-                cube({ size: [params.unitSize * totalWidth + params.unitSize, params.unitSize * 4, params.unitSize * 0.5] }),
-
-                // Top boxes
-                cube({ size: [spacerWidth, params.unitSize * 0.5, params.unitSize] }).translate([0, params.unitSize * 4 - params.unitSize * 0.5, 0]),
-                cube({ size: [params.spaceBetweenPins, params.unitSize * 0.5, params.unitSize] }).translate([spacerWidth + params.unitSize, params.unitSize * 4 - params.unitSize * 0.5, 0]),
-                cube({ size: [spacerWidth, params.unitSize * 0.5, params.unitSize] }).translate([rightPinStart, params.unitSize * 4 - params.unitSize * 0.5, 0]),
-
-                // bottom boxes
-                difference(
-                    cube({ size: [spacerWidth, params.unitSize * 3, params.unitSize * 1.5] }).translate([0, 0, 0]),
-                    cube({ size: [params.unitSize * 0.5, params.unitSize * 2, params.unitSize * 3] }).translate([spacerWidth - 0.5, params.unitSize / 2, 0])
-                ),
-
-                // center box
-                cube({ size: [params.unitSize * params.spaceBetweenPins, params.unitSize * 3, params.unitSize * 1.5] }).translate([spacerWidth + params.unitSize, 0, 0]),
-
-                difference(
-                    cube({ size: [spacerWidth, params.unitSize * 3, params.unitSize * 1.5] }).translate([rightPinStart, 0, 0]),
-                    cube({ size: [params.unitSize * 0.5, params.unitSize * 2, params.unitSize * 3] }).translate([rightPinStart, params.unitSize / 2, 0])
-                )
-            )
+            getData(params,totalWidth)
         ).translate([i * params.unitSize * totalWidth, 0, 0]);
         records.push(row);
     }
