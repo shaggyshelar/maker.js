84ff578f3ec3156dd689b21c6b0951bf9fe29764
shaggyshelar <shaggy.shelar@gmail.com>
2019-06-15 14:37:04 +0530

    Completed side box

--- docs/demos/js/3Pin/main-base-box.js
+++ docs/demos/js/3Pin/main-base-box.js
@@ -7,8 +7,8 @@ var xValue = 2.5;
 function getParameterDefinitions() {
     return [
         { name: 'unitSize', type: 'int', initial: 1, caption: 'Unit Size' },
-        { name: 'leftCellColor', type: 'color', initial: '#F6D55C', caption: 'Row Base Color' },
-        { name: 'rightCellColor', type: 'color', initial: '#ED553B', caption: 'Row Movable Base Color' },
+        { name: 'cellColor', type: 'color', initial: '#ED553B', caption: 'Row Base Color' },
+        { name: 'baseColor', type: 'color', initial: '#F6D55C', caption: 'Row Movable Base Color' },
         { name: 'pinColor', type: 'color', initial: '#173F5F', caption: 'Pin Color' },
         { name: 'pushPinColor', type: 'color', initial: '#3CAEA3', caption: 'Push Pin Color' },
         { name: 'rowClosePanelColor', type: 'color', initial: '#F6D55C', caption: 'Row Close Panel Color' },
@@ -20,21 +20,22 @@ function getParameterDefinitions() {
     ];
 }
 
-function getBase(params) {
-    var singleCellYValue = params.isTwoPin ? twoPinYValue : threePinYValue;
+function getColumns(params) {
+    var pinYValue = params.isTwoPin ? twoPinYValue : threePinYValue;
+    var singleCellYValue = params.isTwoPin ? pinYValue : threePinYValue;
     var singleColumnWidth = 2 * xValue + params.spaceBetweenColumnCells + params.spaceBetweenColumns;
 
     var columns = []
 
     for (var i = 0; i < params.numberOfColumns; i++) {
         var leftColumnCell = color(
-            html2rgb(params.leftCellColor),
+            html2rgb(params.cellColor),
             union(cube({ size: [params.unitSize * xValue, params.unitSize * params.numberOfRows * singleCellYValue, params.unitSize * zValueWithoutPinHeight] })).translate([i * singleColumnWidth, 0, 0])
         );
         columns.push(leftColumnCell);
     
         var rightColumnCell = color(
-            html2rgb(params.rightCellColor),
+            html2rgb(params.cellColor),
             cube({ size: [params.unitSize * xValue, params.unitSize * params.numberOfRows * singleCellYValue, params.unitSize * zValueWithoutPinHeight] }).translate([i * singleColumnWidth + params.unitSize * xValue + params.spaceBetweenColumnCells, 0, 0])
         );
         columns.push(rightColumnCell);
@@ -42,13 +43,48 @@ function getBase(params) {
     return columns;
 }
 
+function getBaseBox(params) {
+    var pinYValue = params.isTwoPin ? twoPinYValue : threePinYValue;
+    var singleCellYValue = params.isTwoPin ? pinYValue : threePinYValue;
+    var singleColumnWidth = 2 * xValue + params.spaceBetweenColumnCells + params.spaceBetweenColumns;
+
+    return color(
+      html2rgb(params.baseColor),
+      union(
+        cube({
+          size: [
+            params.unitSize * xValue,
+            params.unitSize * params.numberOfRows * singleCellYValue,
+            params.unitSize * zValueWithoutPinHeight
+          ]
+            }).translate([(params.spaceBetweenColumns + xValue) * -1, 0, 0]),
+        cube({
+            size: [
+              params.unitSize * xValue,
+              params.unitSize * params.numberOfRows * singleCellYValue,
+              params.unitSize * zValueWithoutPinHeight
+            ]
+          }).translate([(singleColumnWidth * params.numberOfColumns ), 0, 0]),
+          cube({
+            size: [
+                singleColumnWidth * params.numberOfColumns + singleColumnWidth - params.unitSize,
+                params.unitSize,
+                params.unitSize * zValueWithoutPinHeight
+            ]
+          }).translate([(params.spaceBetweenColumns + xValue) * -1, params.numberOfRows * pinYValue, 0]),
+          cube({
+            size: [
+                singleColumnWidth * params.numberOfColumns + singleColumnWidth - params.unitSize,
+                params.unitSize,
+                params.unitSize * zValueWithoutPinHeight
+            ]
+          }).translate([(params.spaceBetweenColumns + xValue) * -1, params.unitSize * -1, 0])
+      )
+    );
+}
+
 function main(params) {
-    var mainBase = getBase(params);
-    // var singleCellYValue = params.isTwoPin ? twoPinYValue : threePinYValue;
-    // var singleColumnWidth = 2 * xValue + params.spaceBetweenColumnCells + params.spaceBetweenColumns;
-    // var mainBase = color(
-    //     html2rgb(params.leftCellColor),
-    //     union(cube({ size: [params.unitSize * xValue, params.unitSize * params.numberOfRows * singleCellYValue, params.unitSize * zValueWithoutPinHeight] }))
-    // );
-    return mainBase;
+    var mainBase = getColumns(params);
+    var baseBox = getBaseBox(params);
+    return mainBase.concat(baseBox);
 }
