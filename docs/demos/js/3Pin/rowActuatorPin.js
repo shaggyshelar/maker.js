492daeae912a75cc59b9617716128a1d319641ba
shaggyshelar <shaggy.shelar@gmail.com>
2019-06-22 17:02:24 +0530

    Completed row actuator pin

--- docs/demos/js/3Pin/rowActuatorPin.js
+++ docs/demos/js/3Pin/rowActuatorPin.js
@@ -8,7 +8,7 @@ function main(params) {
     var yValue = 13.4;
     var zValue = 1;
     var movableSpaceWidth = 7.8;
-    var movableSpaceHeight = 2.4;
+    var movableSpaceHeight = 2.6;
     var movableSpaceStartX = (10 - movableSpaceWidth) / 2;
 
     return color(html2rgb(params.rowActuatorColor),
@@ -16,10 +16,13 @@ function main(params) {
         difference(
             //base
             cube({ size: [xValue, yValue, zValue] }),
-            cube({ size: [10, yValue, zValue] }).translate([0, 3.4, 0]),
-            cube({ size: [movableSpaceWidth, movableSpaceHeight, zValue] }).translate([movableSpaceStartX, 0.5, 0])
+            cube({ size: [10, yValue, zValue] }).translate([0, -3.4, 0]),
+            cube({ size: [movableSpaceWidth, movableSpaceHeight, zValue] }).translate([movableSpaceStartX, 10 + 0.6, 0])
         ),
-        cube({ size: [2, 2, 5] }).translate([xValue, 7,0]),
+        difference(
+            cube({ size: [2, 2, 7] }).translate([xValue, 5,0]),
+            cube({ size: [1, 2, 1] }).translate([xValue, 5,3]),
+        )
         )
     );
 }
